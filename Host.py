7c106c375a9e80f332aeb783f392647a4d60ced5
ryanlid87 <rlidster1@gmail.com>
2016-01-17 17:20:34 -0600

    Update Host.py

--- Host.py
+++ Host.py
@@ -5,10 +5,15 @@ host = socket.gethostname()
 port = 8082
 s.bind(('',port))
 s.listen(5)
+data = 'Welcome to the chat'
 
 while True:
     c, addr = s.accept()
     print 'Got Connection from', addr
-    s.send('peter')
+    c.send(data)
+    while data != 'q' or data != 'Q':
+        data = c.recv(1024)
+        print data
+        c.send(data)
     c.close()
 
