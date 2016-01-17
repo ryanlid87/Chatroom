87e7be181c240ec9bb676048d84e8d3971f4268d
ryanlid87 <rlidster1@gmail.com>
2016-01-17 17:01:38 -0600

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,10 +1,21 @@
 import socket
-max_char_len = 1024
 
 s = socket.socket()
 host = socket.gethostname()
-port = 8081
-
+port = 8082
+data = ''
 s.connect(('71.9.176.217',port))
-print s.recv(max_char_len)
-s.close
+while 1:
+    data = s.recv(512)
+    if ( data == 'q' or data == 'Q'):
+        s.close()
+        break
+    else:
+        print "RECIEVED:" , data
+        data = raw_input ( "SEND( TYPE q or Q to Quit):" )
+        if (data <> 'Q' and data <> 'q'):
+            s.send(data)
+        else:
+            s.send(data)
+            s.close()
+            break
